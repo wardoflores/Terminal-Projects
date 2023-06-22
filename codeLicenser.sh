20d31051b9399ec3a45ba5146063c6da8f459791
wardoflores <eduardojoeyflores@gmail.com>
2023-06-23 05:22:52 +0800

    Create codeLicenser.sh

---
+++ codeLicenser.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# This {language} file uses the following encoding: utf-8
+# {module name}
+# {project name}: {github link}
+# Created YYYY-MM-DD
+# Copyright (c) 2023 Wardo Flores
+#
+# This script adds the license alike the first 6 lines of this shell script. TODO
