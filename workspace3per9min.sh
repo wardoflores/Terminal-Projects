fa95ae352fe20105c6e5996812d1b99695c3411d
wardoflores <eduardojoeyflores@gmail.com>
2023-06-23 05:22:48 +0800

    Create workspace3per9min.sh

---
+++ workspace3per9min.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# This {language} file uses the following encoding: utf-8
+# {module name}
+# {project name}: {github link}
+# Created 2023-05-19
+# Copyright (c) 2023 Wardo Flores
+#
+# watch -n540 -x ./GitHub/Terminal-Projects/workspace3per9min.sh
+
+{ echo key super+3; echo key ctrl+tab; } | dotool
