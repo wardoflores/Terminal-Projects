aaf9c32bf94a7ad9dfdc49dcd393ac45136efa37
wardoflores <flyingahas@kulmata.com>
2024-11-27 04:09:10 +0800

    opis: Pulling all branch for main.

---
+++ opisgitter.sh
@@ -0,0 +1,15 @@
+cd ~/storage/shared/repository/Notes || exit
+git add .
+git commit -a -m "opis: Pulling all branch for main."
+git push
+
+cd ~/storage/shared/repository/ShellScripts 
+git add .
+git commit -a -m "opis: Pulling all branch for main."
+git push
+
+cd ~/storage/shared/repository/Dotfiles 
+git add .
+git commit -a -m "opis: Pulling all branch for main."
+git push
+
