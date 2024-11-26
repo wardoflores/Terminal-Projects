0f39f518afb584087600e55aa084a298c2874244
wardoflores <eduardojosefloresiii@gmail.com>
2024-11-26 19:54:57 +0000

    takarabune: Pulling all branch for main.

---
+++ takarabunegitter.sh
@@ -0,0 +1,24 @@
+cd ~/repository/Notes || exit
+git add .
+git commit -a -m "takarabune: Pulling all branch for main."
+git push
+git pull origin imoogi
+git pull origin opis
+git pull origin leviathan
+
+cd ~/repository/ShellScripts 
+git add .
+git commit -a -m "takarabune: Pulling all branch for main."
+git push
+git pull origin imoogi
+git pull origin opis
+git pull origin leviathan
+
+cd ~/repository/Dotfiles 
+git add .
+git commit -a -m "takarabune: Pulling all branch for main."
+git push
+git pull origin imoogi
+git pull origin opis
+git pull origin leviathan
+
